735b370578d60526c3b0071020f73e1f11a319b8
soup-bowl <code@soupbowl.io>
2022-05-11 07:01:28 +0000

    Begone, spaces!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Version {process.env.REACT_APP_VERSION}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Edit <code>src/App.tsx</code> and save to reload. Version {process.env.REACT_APP_VERSION}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
