925184ec4849716825f546d1b3c9cd2a418ff6b2
soup-bowl <code@soupbowl.io>
2022-08-22 07:54:20 +0000

    Non-WP site or error handler.

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider, Toolbar, IconButton, Typography,
 	Container, styled, Drawer, Divider, Box, List, ListItemIcon,
 	ListItemText, useMediaQuery, ListItemButton} from '@mui/material';
 import { useEffect, useState } from "react";
+import { AxiosError } from "axios";
 import theme from '../theme';
 import agent from "../agent";
 import { ISiteInformation } from "../interfaces";
@@ -88,8 +89,8 @@ export default function Layout() {
 				hasPosts: false
 			});
         })
-		.catch((err:any) => {
-			setApiError('An error has occurred: ');
+		.catch((err:AxiosError) => {
+			setApiError(`[${err.code}] ${err.message}`);
 			setMainInfo({} as ISiteInformation);
 		});
     }, [inputURL]);
@@ -169,7 +170,23 @@ export default function Layout() {
 						{apiError === '' ?
 						<Outlet context={[mainInfo]}  />
 						:
-						<Typography>{apiError}</Typography>
+						<>
+							<Typography variant="h1">Unable to Display Website</Typography>
+							<Typography my={2}>
+								We've made a request to the website's JSON API (if it exists), and have not had the
+								expected response returned.
+							</Typography>
+							<Typography my={2}>
+								This can be due to the following reasons:
+							</Typography>
+							<ul>
+								<li>The website you requested is not a WordPress site.</li>
+								<li>The website has blocked or disabled their REST API endpoint(s).</li>
+								<li>The API is behind a strict CORS policy disabling us from seeing it.</li>
+							</ul>
+							<Typography variant="h5" component="h2">Technical Details</Typography>
+							<Typography my={2} sx={{fontFamily: 'monospace'}}>{apiError}</Typography>
+						</>
 						}
 					</Container>
 				</Main>
