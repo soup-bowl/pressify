a4a94f46eb5db2b5f5529ac1977308e657b0f0b2
soup-bowl <code@soupbowl.io>
2022-08-29 21:17:10 +0000

    Keep menu items in 1 place.

---
+++ src/components/menu.tsx
@@ -0,0 +1,72 @@
+import { Divider, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import HomeIcon from '@mui/icons-material/Home';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import DescriptionIcon from '@mui/icons-material/Description';
+import CoPresentIcon from '@mui/icons-material/CoPresent';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+
+interface Props {
+	onClose: any;
+	theme: any;
+	colorMode: any;
+}
+
+export default function MenuItems({onClose, theme, colorMode}:Props) {
+	const navigate = useNavigate();
+	const { inputURL } = useParams();
+
+	return(
+		<>
+		<List>
+			<ListItemButton
+				onClick={() => {navigate(`/${inputURL}`);onClose();}}
+				selected={(window.location.hash.replace(`/${inputURL}`, '') === "#")}
+			>
+				<ListItemIcon><HomeIcon /></ListItemIcon>
+				<ListItemText primary="Home" />
+			</ListItemButton>
+		</List>
+		<Divider />
+		<List>
+			<ListItemButton
+				onClick={() => {navigate(`/${inputURL}/posts`);onClose();}}
+				selected={window.location.hash.includes("/posts")}
+			>
+				<ListItemIcon><PushPinIcon /></ListItemIcon>
+				<ListItemText primary="Posts" />
+			</ListItemButton>
+			<ListItemButton
+				onClick={() => {navigate(`/${inputURL}/pages`);onClose();}}
+				selected={window.location.hash.includes("/pages")}
+			>
+				<ListItemIcon><DescriptionIcon /></ListItemIcon>
+				<ListItemText primary="Pages" />
+			</ListItemButton>
+		</List>
+		<Divider />
+		<List>
+			<ListItemButton onClick={() => {navigate('/');onClose();}}>
+				<ListItemIcon><KeyboardReturnIcon /></ListItemIcon>
+				<ListItemText primary="Change Site" />
+			</ListItemButton>
+			<ListItemButton onClick={colorMode.toggleColorMode}>
+				<ListItemIcon>
+					{theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+				</ListItemIcon>
+				<ListItemText primary={theme.palette.mode === 'dark' ? 'Light Mode' : 'Dark Mode'} />
+			</ListItemButton>
+			<ListItemButton
+				onClick={() => {navigate(`/${inputURL}/about`);onClose();}}
+				selected={window.location.hash.includes("/about")}
+			>
+				<ListItemIcon><CoPresentIcon /></ListItemIcon>
+				<ListItemText primary="About" />
+			</ListItemButton>
+		</List>
+		</>
+	);
+}
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,24 +1,17 @@
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import { CssBaseline, ThemeProvider, Toolbar, IconButton, Typography,
-	Container, styled, Drawer, Divider, Box, List, ListItemIcon,
-	ListItemText, useMediaQuery, ListItemButton, alpha, InputBase,
+	Container, styled, Drawer, Divider, Box, useMediaQuery, alpha, InputBase,
 	createTheme, PaletteMode } from '@mui/material';
 import { createContext, useEffect, useMemo, useState } from "react";
 import { green } from '@mui/material/colors';
 import { ISiteInfo } from "../interfaces";
 import WPAPI from "wpapi";
+import MenuItems from "../components/menu";
 import { PrincipalAPIError } from "../components/error";
 
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import HomeIcon from '@mui/icons-material/Home';
-import PushPinIcon from '@mui/icons-material/PushPin';
-import DescriptionIcon from '@mui/icons-material/Description';
-import CoPresentIcon from '@mui/icons-material/CoPresent';
-import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 
 const drawerWidth = 240;
 
@@ -271,52 +264,7 @@ export default function Layout({simple = false}:Props) {
 							</IconButton>
 						</DrawerHeader>
 						<Divider />
-						<List>
-							<ListItemButton
-								onClick={() => {navigate(`/${inputURL}`);handleDrawerClose();}}
-								selected={(window.location.hash.replace(`/${inputURL}`, '') === "#")}
-							>
-								<ListItemIcon><HomeIcon /></ListItemIcon>
-								<ListItemText primary="Home" />
-							</ListItemButton>
-						</List>
-						<Divider />
-						<List>
-							<ListItemButton
-								onClick={() => {navigate(`/${inputURL}/posts`);handleDrawerClose();}}
-								selected={window.location.hash.includes("/posts")}
-							>
-								<ListItemIcon><PushPinIcon /></ListItemIcon>
-								<ListItemText primary="Posts" />
-							</ListItemButton>
-							<ListItemButton
-								onClick={() => {navigate(`/${inputURL}/pages`);handleDrawerClose();}}
-								selected={window.location.hash.includes("/pages")}
-							>
-								<ListItemIcon><DescriptionIcon /></ListItemIcon>
-								<ListItemText primary="Pages" />
-							</ListItemButton>
-						</List>
-						<Divider />
-						<List>
-							<ListItemButton onClick={() => {navigate('/');handleDrawerClose();}}>
-								<ListItemIcon><KeyboardReturnIcon /></ListItemIcon>
-								<ListItemText primary="Change Site" />
-							</ListItemButton>
-							<ListItemButton onClick={colorMode.toggleColorMode}>
-								<ListItemIcon>
-									{theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-								</ListItemIcon>
-								<ListItemText primary={theme.palette.mode === 'dark' ? 'Light Mode' : 'Dark Mode'} />
-							</ListItemButton>
-							<ListItemButton
-								onClick={() => {navigate(`/${inputURL}/about`);handleDrawerClose();}}
-								selected={window.location.hash.includes("/about")}
-							>
-								<ListItemIcon><CoPresentIcon /></ListItemIcon>
-								<ListItemText primary="About" />
-							</ListItemButton>
-						</List>
+						<MenuItems onClose={handleDrawerClose} colorMode={colorMode} theme={theme} />
 					</Drawer>
 					</>
 					: null }
