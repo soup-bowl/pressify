7cc4edaa2db0e104204b02481c7ce758dafa1655
soup-bowl <code@soupbowl.io>
2024-03-03 22:45:10 +0000

    Don't spawn new domain (fixes Issue with routing #81)

--- src/components/siteSelector.tsx
+++ src/components/siteSelector.tsx
@@ -2,7 +2,7 @@ import {
 	ButtonGroup, FormControl, Grid, IconButton, InputAdornment, InputLabel,
 	List, ListItem, ListItemButton, OutlinedInput, Typography
 } from "@mui/material";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, FormEventHandler, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppDialog } from ".";
 import { useLocalStorageJSON } from "@/localStore";
@@ -48,9 +48,10 @@ export const SiteSelector = ({ onClose = undefined }: SiteSelectorProps) => {
 	const [historic, setHistoric] = useLocalStorageJSON<string[]>(localStorageRefs.history, []);
 	const [saved, setSaved] = useLocalStorageJSON<string[]>(localStorageRefs.saved, []);
 
-	const submitForm = () => {
+	const submitForm: FormEventHandler<HTMLFormElement> = (e) => {
+		e.preventDefault();
 		if (searchValue !== undefined && searchValue !== "") { saveSiteToHistory(searchValue) }
-		navigate('/' + searchValue);
+		navigate(`/${searchValue}`);
 		if (onClose !== undefined) { onClose() }
 	};
 
