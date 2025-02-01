96d7d50ad7170376f99a81750faaed890c8104d0
soup-bowl <code@soupbowl.io>
2025-02-01 22:47:21 +0000

    Devcontainer adjustments.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 docker-compose -f .github/docker-compose.yml pull
 docker-compose -f .github/docker-compose.yml build
-npm install
+npm ci
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
-echo "PUBLIC_URL=https://${CODESPACE_NAME}-3000.preview.app.github.dev" > .env.development.local
+if [ ! -z ${GITPOD_HOST+x} ]; then
+	ACCESS_URL="https://8100-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}"
+elif [ ! -z ${CODESPACE_NAME+x} ]; then
+	ACCESS_URL="https://${CODESPACE_NAME}-8100.preview.app.github.dev"
+else
+	ACCESS_URL="http://localhost:8100"
+fi
+
+echo "PUBLIC_URL=${ACCESS_URL}" > .env.development.local
 echo "REACT_APP_VERSION=\$npm_package_version" >> .env.development.local
 cp .env.development.local .env.production.local
